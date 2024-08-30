e7e959fa2493771c827513043ca0e37f836a2010
srirampamerla <53964156+srirampamerla@users.noreply.github.com>
2024-08-30 10:32:31 +0530

    Add files via upload

---
+++ 1.1-Basics of Langchain/serve.py
@@ -0,0 +1,50 @@
+from fastapi import FastAPI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_groq import ChatGroq
+from langserve import add_routes
+
+
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+groq_api_key=os.getenv('GROQ_API_KEY')
+model=ChatGroq(model="Gemma2-9b-It",groq_api_key=groq_api_key)
+
+#1. Create prompt template
+system_template= "Translate the following into {language}:"
+prompt_template= ChatPromptTemplate.from_messages(
+    [
+        ("system",system_template),
+        ("user","{text}")
+    ]
+)
+
+parser=StrOutputParser()
+
+#create the chain
+chain=prompt_template|model|parser
+
+
+# App definition
+app=FastAPI(title="Langchain Server",
+            version="1.0",
+            description="A simple API server using Langchain runnable interfaces")
+
+# Adding Chain routes
+add_routes(
+    app,
+    chain,
+    path="/chain"
+)
+
+if __name__=="__main__":
+    import uvicorn #Uvicorn is a fast ASGI (Asynchronous Server Gateway Interface)
+    uvicorn.run(app,host="localhost",port=8000)
+
+
+# run-- python serve.py
+# open the link after portnumber write /docs
+#open the link after portnumber write /chain/input_schema
+# we can get multiple paths we will see in the documentation path.
